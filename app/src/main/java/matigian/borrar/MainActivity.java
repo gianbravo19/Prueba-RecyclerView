494ae127f6911da26f9abb1ba3fd5573d9906019
Francisco Cabezas <francisco.cabezas@accedo.tv>
2015-12-17 15:51:34 -0300

    - Se resuelven problemas de declaración

--- app/src/main/java/matigian/borrar/MainActivity.java
+++ app/src/main/java/matigian/borrar/MainActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
+
+    private List<Person> persons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,17 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        initializeData();
 
+    }
 
+    // This method creates an ArrayList that has three Person objects
+    // Checkout the project associated with this tutorial on Github if
+    // you want to use the same images.
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old"));
+        persons.add(new Person("Lavery Maiss", "25 years old"));
+        persons.add(new Person("Lillie Watts", "35 years old"));
     }
 }
--- app/src/main/java/matigian/borrar/Person.java
+++ app/src/main/java/matigian/borrar/Person.java
@@ -10,22 +10,8 @@ public class Person {
     public String name;
     public String age;
 
-
     Person(String name, String age) {
         this.name = name;
         this.age = age;
-
     }
 }
-
-private List<Person> persons;
-
-    // This method creates an ArrayList that has three Person objects
-// Checkout the project associated with this tutorial on Github if
-// you want to use the same images.
-    private void initializeData(){
-        persons = new ArrayList<>();
-        persons.add(new Person("Emma Wilson", "23 years old"));
-        persons.add(new Person("Lavery Maiss", "25 years old"));
-        persons.add(new Person("Lillie Watts", "35 years old"));
-    }
